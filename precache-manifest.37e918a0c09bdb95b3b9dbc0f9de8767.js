297d8e3faba663d0501075eb2103ac68c1f66cde
IanKim <iankim2280@gmail.com>
2020-09-05 02:37:06 -0400

    Updates

---
+++ precache-manifest.37e918a0c09bdb95b3b9dbc0f9de8767.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d47954e9e8e3315eb082e7d1cd4206e",
+    "url": "/iankim2280.github.io/airbnb-clone-frontend/index.html"
+  },
+  {
+    "revision": "9e41944669b89ec3c780",
+    "url": "/iankim2280.github.io/airbnb-clone-frontend/static/css/main.03497b0b.chunk.css"
+  },
+  {
+    "revision": "f4407bc3ff84d843d718",
+    "url": "/iankim2280.github.io/airbnb-clone-frontend/static/js/2.5b1fe2e6.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/iankim2280.github.io/airbnb-clone-frontend/static/js/2.5b1fe2e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e41944669b89ec3c780",
+    "url": "/iankim2280.github.io/airbnb-clone-frontend/static/js/main.f7a5e198.chunk.js"
+  },
+  {
+    "revision": "c1138cdecb689d331518",
+    "url": "/iankim2280.github.io/airbnb-clone-frontend/static/js/runtime-main.fb27aea8.js"
+  },
+  {
+    "revision": "a6f1ed9f6af33fce80b04446c18fcb55",
+    "url": "/iankim2280.github.io/airbnb-clone-frontend/static/media/logo.a6f1ed9f.png"
+  }
+]);
